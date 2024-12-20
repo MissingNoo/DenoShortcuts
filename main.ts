176a1995abd78e257b533008e8181e0cd1433edc
Airgeadlamh <eduaraguia@gmail.com>
2024-12-20 20:14:40 -0300

    Fixes

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import * as util from "node:util";
 import * as child_process from "node:child_process";
 import commands from "./commands.json" with { type: "json" };
 
-export function exec(command: string){
+export function exec(command: string, show_output = false){
   child_process.exec(command, (err, stdout, stderr) => {
     if (err) {
       // node couldn't execute the command
@@ -10,25 +10,27 @@ export function exec(command: string){
     }
   
     // the *entire* stdout and stderr (buffered)
-    console.log(`${stdout}`);
+    if (show_output) {
+      console.log(`${stdout}`);
+    }
     //console.log(`stderr: ${stderr}`);
   });
 }
 
 commands.elements.forEach(element => {
-  let basehtml = '<!DOCTYPE html><html lang="en"><head>  <meta charset="UTF-8">    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">  <title>WebSocket Demo</title>  </head><style> a.button {    padding: 1px 6px;    border: 1px outset buttonborder;    border-radius: 3px;    color: buttontext;    background-color: buttonface;    text-decoration: none;}  body {    background-color: black;  }  button {    margin-top: 5px;    padding: 29px;    background-color: black;    color: white;  }</style><body>'
+  let basehtml = '<!DOCTYPE html><html lang="en"><head>  <meta charset="UTF-8">    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">  <title>WebSocket Demo</title>  </head><style> a.button {    padding: 1px 6px;    border: 1px outset buttonborder;    border-radius: 3px;    color: buttontext;    background-color: buttonface;    text-decoration: none;}  body {    background-color: black;  }  button {    margin-top: 5px;    padding: 29px;    background-color: black;    color: white;  }</style><body>\n'
   if (element.name != "Home") {
-    basehtml = basehtml + '<a href="Home" class="button">Back</a>';
+    basehtml = basehtml + '<a href="Home" class="button">Back</a>\n';
   }
   element.buttons.forEach(element => {
     let style = "";
     if (element.image != undefined) {
-      style = 'style="width : 100px; height : 74px;  background: url(static/' + element.image + '); background-repeat: no-repeat; background-size: auto 100%; background-position: center center;"';
-      element.name = "";
+      style = 'style="width : 100px; height : 74px;  background: url(static/' + element.image + '); background-repeat: no-repeat; background-size: auto 100%; background-position: center center; color: transparent;"';
+      //element.name = "";
     }
     switch (element.type) {
       case "Folder":
-        basehtml = basehtml + '<a href="' + element.name + '" class="button">' + element.name + '</a>';
+        basehtml = basehtml + '<a href="' + element.name + '" class="button">' + element.name + '</a>\n';
         break;
       default:
         basehtml = basehtml + '<button ' + style + ' id="' + element.name + '"> ' + element.name + ' </button>\n';
@@ -50,7 +52,7 @@ commands.elements.forEach(element => {
   Deno.writeTextFile("pages/" + element.name + ".html", basehtml);
 });
 
-exec('qrencode -m 2 -t utf8 "http://192.168.0.105:8000"');
+exec('qrencode -m 2 -t utf8 "http://192.168.0.105:8000"', true);
 Deno.serve((req) => {
   let body;
   const url = new URL(req.url);
